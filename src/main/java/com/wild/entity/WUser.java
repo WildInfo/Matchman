270a950b99ce4be9b61b60b5131b808213abbc0f
Administrator <Administrator@CJH>
2017-04-07 16:09:51 +0800

    cjh

--- src/main/java/com/wild/entity/WUser.java
+++ src/main/java/com/wild/entity/WUser.java
@@ -23,6 +23,24 @@ public class WUser implements Serializable {
 	private int WStatus;// 用户状态
 	private int WSuperManager;// 用户角色
 
+	public WUser(String wID, String wName, String wSex, String wUserNum, String wPassWord, String wAge, Date wDate,
+			int wStatus, int wSuperManager) {
+		super();
+		WID = wID;
+		WName = wName;
+		WSex = wSex;
+		WUserNum = wUserNum;
+		WPassWord = wPassWord;
+		WAge = wAge;
+		WDate = wDate;
+		WStatus = wStatus;
+		WSuperManager = wSuperManager;
+	}
+
+	public WUser() {
+		super();
+	}
+
 	public String getWID() {
 		return WID;
 	}
